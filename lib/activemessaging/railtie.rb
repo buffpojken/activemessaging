4b593ddd1b7b7e8e6b487e748565e38b4dbb8ef9
buffpojken <daniel@sykewarrior.com>
2010-11-19 16:31:04 +0100

    Rails-initializer now loads with correct path

--- lib/activemessaging/railtie.rb
+++ lib/activemessaging/railtie.rb
@@ -7,7 +7,7 @@ module ActiveMessaging
   class Railtie < Rails::Railtie
     
     initializer 'i.wonder.what.this.is' do
-   
+   
 
         path = File.expand_path("#{Rails.root}/config/messaging.rb")
         begin
@@ -23,13 +23,10 @@ module ActiveMessaging
         load Rails.root + '/app/processors/application.rb' if File.exist?("#{Rails.root}/app/processors/application.rb")
         Dir[Rails.root + '/app/processors/*.rb'].each do |f|
           unless f.match(/\/application.rb/)
-            logger.debug "ActiveMessaging: Loading #{f}" if first
+            logger.debug "ActiveMessaging: Loading #{f}"
             load f
           end
         end
-
-
-
     end
     
     
